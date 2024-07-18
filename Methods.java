f468847e13f407966b4033200663d8328bcb638f
skybloom4000 <150357917+skybloom4000@users.noreply.github.com>
2024-07-18 13:42:13 +0300

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_methods;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author grace
+ */
+public class Methods {    
+ 
+    public static void findLargestAndSmallest() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int largest, smallest;
+
+        // Find the largest number
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        // Find the smallest number
+        if (num1 <= num2 && num1 <= num3) {
+            smallest = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            smallest = num2;
+        } else {
+            smallest = num3;
+        }
+
+        System.out.println("The smallest number: " + smallest);
+        System.out.println("The largest number: " + largest);
+        System.out.println(largest + " is your largest and " + smallest + " is your smallest number");
+    }
+
+    public static void main(String[] args) {
+        findLargestAndSmallest();
+    }
+}
+
+    
+}
